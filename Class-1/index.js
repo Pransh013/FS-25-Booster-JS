628db0e38a38f881f13f0b9d996a8db530c14d4d
Pranshu Verma <pranshuverma1601@gmail.com>
2024-12-27 23:02:07 +0530

    init

---
+++ Class-1/index.js
@@ -0,0 +1,35 @@
+// var a;
+// var b;
+// var c;
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+// console.log(a + ", " +  b + ", " + c)
+
+// let num; // variables declared using let are block scoped and cannot be redeclared
+// num = 20;
+// // let num = 10; //
+// const a = 10;
+// // a = 15; // variables declared using const are block scoped and cannot be redeclared, reassigned
+// console.log(num, a);
+
+// ``  //Backticks
+
+// let obj = {
+//   name: "John",
+//   age: 30,
+//   address: {
+//     street: "123 Main St",
+//     city: "New York",
+//     state: "NY",
+//   }
+// };
+
+let x;
+x = x + 10;
+let z = null;
+z = z - 10;
+
+console.log(x, z);
+
---
+++ Class-2 Patterns/index.js
@@ -0,0 +1,86 @@
+function printPatternI(n) {
+  let rows = (n + 1) / 2;
+  let st = 1;
+  let sp = rows - 1;
+
+  for (let i = 1; i <= rows; i++) {
+    let ans = "";
+    for (let j = 1; j <= sp; j++) {
+      ans += "  ";
+    }
+
+    for (let j = 1; j <= st; j++) {
+      ans += j + " ";
+    }
+    st += 2;
+    sp--;
+    console.log(ans);
+  }
+}
+
+function printPatternII(n) {
+  let rows = 2 * n - 1;
+  let st = 1;
+  let sp = n - 1;
+
+  for (let i = 1; i <= rows; i++) {
+    let ans = "";
+    for (let j = 1; j <= sp; j++) {
+      ans += "  ";
+    }
+
+    for (let j = 1; j <= st; j++) {
+      ans += j + " ";
+    }
+    if (i < n) {
+      st++;
+      sp--;
+    } else {
+      st--;
+      sp++;
+    }
+    console.log(ans);
+  }
+}
+
+function printPatternIII(n) {
+  let rows = 2 * n - 1;
+  let st = 1;
+  for (let i = 1; i <= rows; i++) {
+    let ans = "";
+    for (let j = st; j >= 1; j--) {
+      ans += j + " ";
+    }
+    if (i < n) {
+      st++;
+    } else {
+      st--;
+    }
+    console.log(ans);
+  }
+}
+
+function printPatternIV(n) {
+  let rows = (n + 1) / 2;
+  let st = n;
+  let sp = 0;
+
+  for (let i = 1; i <= rows; i++) {
+    let ans = "";
+    for (let j = 1; j <= sp; j++) {
+      ans += "  ";
+    }
+
+    for (let j = 1; j <= st; j++) {
+      ans += j + " ";
+    }
+    st -= 2;
+    sp++;
+    console.log(ans);
+  }
+}
+
+printPatternI(11);
+printPatternII(5);
+printPatternIII(5);
+printPatternIV(9);
